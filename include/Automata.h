72a132f8108d923e51101531989cd960d3a4c6c7
Makar <anl-pr@yandex.ru>
2022-04-20 09:24:55 +0300

    comment

--- include/Automata.h
+++ include/Automata.h
@@ -19,20 +19,21 @@ enum STATES {
 };
 
 class Automata {
-       public:
-       int cash;
-       STATES state;
-       const vector<string> menu = { "coffee", "tea", "cocoa"};
-       const vector<int> prices = {100, 70, 80 };
-       Automata();
-       void on();
-       void off();
-       void coin(int money);
-       vector<string> getMenu();
-       STATES getStates();
-       int choice(string drink);
-       bool check(int index);
-       int cancel();
-       void cook(int index);
-       int finish();
+     private:
+        int cash;
+        STATES state;
+        const vector<string> menu = { "coffee", "tea", "cocoa"};
+        const vector<int> prices = {100, 70, 80 };
+     public:
+        Automata();
+        void on();
+        void off();
+        void coin(int money);
+        vector<string> getMenu();
+        STATES getStates();
+        int choice(string drink);
+        bool check(int index);
+        int cancel();
+        void cook(int index);
+        int finish();
 };
